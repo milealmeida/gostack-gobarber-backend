9a8626c0cd4b847237a7e10feb09291d2d5215de
Milena <milenaalmeidadev@hotmail.com>
2021-01-09 19:58:47 -0400

    Gerando token JWT

--- src/services/AuthenticateUserSession.ts
+++ src/services/AuthenticateUserSession.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,7 +30,15 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    return { user };
+    const token = sign({}, 'fefb60b4ffd5bdb700029ac40b375a0f', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
