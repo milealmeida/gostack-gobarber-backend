2ee6b5215647b4c6ac37d06d69fbb277ee276002
Milena <milenaalmeidadev@hotmail.com>
2021-01-29 17:01:47 -0400

    Injeção de dependências

--- gobarber-backend/src/modules/users/infra/http/routes/users.routes.ts
+++ gobarber-backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { container } from 'tsyringe';
+
 import uploadConfig from '@config/upload';
 
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
@@ -14,8 +15,7 @@ const upload = multer(uploadConfig);
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
 
-  const usersRepository = new UsersRepository();
-  const createUser = new CreateUserService(usersRepository);
+  const createUser = container.resolve(CreateUserService);
 
   const user = await createUser.execute({
     name,
@@ -33,8 +33,7 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const usersRepository = new UsersRepository();
-    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
